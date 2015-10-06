90c44a1c1d4f19540c19073a126d0112378b6ee7
yangbin19921024 <741766481@qq.com>
2015-10-06 22:44:47 +0800

    搭建项目框架

---
+++ 新闻/新闻/Classes/News/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  新闻
+//
+//  Created by yb on 15/10/6.
+//  Copyright © 2015年 yb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
